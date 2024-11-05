656df8bae6bca77d6ab3df6e713e178c1afd009d
Lukas <rubymc@gmx.de>
2024-11-05 17:00:49 +0100

    text util

--- src/main/java/net/prismaforge/libraries/strings/Text.java
+++ src/main/java/net/prismaforge/libraries/strings/Text.java
@@ -1,4 +1,40 @@
 package net.prismaforge.libraries.strings;
 
-public class Text {
+import lombok.NonNull;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.key.KeyPattern;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+
+public final class Text {
+    private static final MiniMessage MINI_MESSAGE = MiniMessage.miniMessage();
+
+    private Text() {
+        throw new AssertionError();
+    }
+
+    @NonNull
+    public static Component text(final String message) {
+        return MINI_MESSAGE.deserialize(message).font(fontKey("minecraft:default"));
+    }
+
+    @NonNull
+    public static Component convertImage(final String message, final @KeyPattern String font) {
+        return MINI_MESSAGE.deserialize("<white>" + message + "</white>").font(fontKey(font));
+    }
+
+    @NonNull
+    public static String image(final String emoji, final @KeyPattern String font) {
+        return "<white><font:" + font + ">" + emoji + "</font>";
+    }
+
+    @NonNull
+    public static Component empty() {
+        return Component.empty();
+    }
+
+    @NonNull
+    public static Key fontKey(final @KeyPattern String font) {
+        return Key.key(font);
+    }
 }
---
+++ src/main/java/net/prismaforge/prismahomes/commands/PrismaHomesCommand.java
@@ -0,0 +1,7 @@
+package net.prismaforge.prismahomes.commands;
+
+public class PrismaHomesCommand {
+
+    // /prismahomes import <PLUGIN> -> importiert die Homes von einem anderen Plugin flüssig zu uns
+
+}
