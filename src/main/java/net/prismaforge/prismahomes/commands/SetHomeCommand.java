6aeeac8e712ad59d59f1bd52d73b47bf92408bb7
Lukas <rubymc@gmx.de>
2024-11-06 18:32:02 +0100

    finish plugin v1

--- src/main/java/net/prismaforge/prismahomes/commands/SetHomeCommand.java
+++ src/main/java/net/prismaforge/prismahomes/commands/SetHomeCommand.java
@@ -7,7 +7,12 @@ import lombok.experimental.FieldDefaults;
 import net.prismaforge.libraries.commands.annotations.Command;
 import net.prismaforge.libraries.commands.annotations.SubCommand;
 import net.prismaforge.libraries.commands.annotations.TabCompletion;
+import net.prismaforge.libraries.config.Config;
+import net.prismaforge.libraries.strings.ColorUtil;
 import net.prismaforge.prismahomes.PrismaHomes;
+import net.prismaforge.prismahomes.storage.DataHome;
+import net.prismaforge.prismahomes.storage.DataPlayer;
+import net.prismaforge.prismahomes.utility.LangKey;
 import net.prismaforge.prismahomes.utility.SlotsUtil;
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
@@ -18,51 +23,49 @@ import java.util.concurrent.CompletableFuture;
 
 @Command(name = "sethome")
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-@RequiredArgsConstructor
 public final class SetHomeCommand {
     PrismaHomes plugin;
+    Config config;
+
+    public SetHomeCommand(final PrismaHomes plugin) {
+        this.plugin = plugin;
+        this.config = plugin.getConfiguration();
+    }
 
     @SubCommand(args = "%name%")
     public void onSetHome(final CommandSender sender, final String[] args) {
         if (!(sender instanceof final Player player)) return;
-
-        //load data
-        final var temp = PrismaAPI.getSurvivalHandler().fetchHazel(player.getUniqueId());
-        if (temp.isEmpty()) {
-            MessageUtil.message(player, LangKey.DATA_ERROR, LangKey.PREFIX_SURVIVAL, s -> s.replaceAll("%player%", player.getName()));
-            return;
-        }
-        final SurvivalPlayer data = temp.get();
+        final DataPlayer data = plugin.getStorageHandler().get(player.getUniqueId());
 
         //check if has slots
         final int slots = SlotsUtil.availableSlots(player);
         if (data.homes().size() >= slots) {
-            MessageUtil.message(player, LangKey.HOMES_NO_SLOTS, LangKey.PREFIX_HOMES);
+            player.sendMessage(ColorUtil.colorString(LangKey.PREFIX.translate(config) + LangKey.HOMES_NO_SLOTS.translate(config)));
             return;
         }
 
         final String key = args[0];
         //check if name meets naming conventions (azAZ09-)
         if (!key.matches("^[A-Za-z0-9_.]+$") || key.length() > 20) {
-            MessageUtil.message(player, LangKey.HOMES_INVALID_NAME, LangKey.PREFIX_HOMES);
+            player.sendMessage(ColorUtil.colorString(LangKey.PREFIX.translate(config) + LangKey.HOMES_INVALID_NAME.translate(config)));
             return;
         }
 
         //check if already has a home with this name
         for (final DataHome home : data.homes()) {
             if (key.equalsIgnoreCase(home.key())) {
-                MessageUtil.message(player, LangKey.HOMES_HOME_ALREADY_PRESENT, LangKey.PREFIX_HOMES);
+                player.sendMessage(ColorUtil.colorString(LangKey.PREFIX.translate(config) + LangKey.HOMES_HOME_ALREADY_PRESENT.translate(config)));
                 return;
             }
         }
 
         //create
         final Location location = player.getLocation();
-        final DataHome home = new DataHome(key, location.getWorld().getName(), location.x(), location.y(), location.z(), location.getYaw(), location.getPitch());
+        final DataHome home = new DataHome(key, location.getWorld().getName(), location.getX(), location.getY(), location.getZ(), location.getYaw(), location.getPitch());
         CompletableFuture.runAsync(() -> {
-            MessageUtil.message(player, LangKey.HOMES_NEW_HOME_CREATED, LangKey.PREFIX_HOMES, s -> s.replaceAll("%name%", home.displayName()));
+            player.sendMessage(ColorUtil.colorString(LangKey.PREFIX.translate(config) + LangKey.HOMES_NEW_HOME_CREATED.translate(config, s -> s.replaceAll("%name%", home.displayName()))));
             data.homes().add(home);
-            PrismaAPI.getSurvivalHandler().updateHazel(data);
+            saveSecure(data);
         });
     }
 
@@ -70,5 +73,9 @@ public final class SetHomeCommand {
     @NonNull
     public List<String> getName() {
         return List.of("<Name>");
+    }
+
+    private void saveSecure(final DataPlayer data) {
+        this.plugin.getStorageHandler().save(data);
     }
 }
