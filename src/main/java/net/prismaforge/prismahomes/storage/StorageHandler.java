37a160709daa42c83d8f8a03d72c0c7493aa02f5
Lukas <rubymc@gmx.de>
2024-11-07 10:28:38 +0100

    add cache layer when fetching & writing playerdata. Improves speed significantly

--- src/main/java/net/prismaforge/prismahomes/storage/StorageHandler.java
+++ src/main/java/net/prismaforge/prismahomes/storage/StorageHandler.java
@@ -2,25 +2,37 @@ package net.prismaforge.prismahomes.storage;
 
 import lombok.AccessLevel;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import net.prismaforge.libraries.config.Config;
 import net.prismaforge.prismahomes.PrismaHomes;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
-@RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public final class StorageHandler {
     PrismaHomes plugin;
+    Map<UUID, DataPlayer> cache;
+
+    public StorageHandler(final PrismaHomes plugin) {
+        this.plugin = plugin;
+        this.cache = new HashMap<>();
+    }
 
     @NonNull
     public synchronized DataPlayer get(final UUID uuid) {
+        if (this.cache.containsKey(uuid)) {
+            return this.cache.get(uuid);
+        }
         final Config file = getFile(uuid);
-        return DataPlayer.createFromConfig(file, uuid);
+        final DataPlayer data = DataPlayer.createFromConfig(file, uuid);
+        this.cache.put(uuid, data);
+        return data;
     }
 
     public synchronized void save(final DataPlayer data) {
+        this.cache.put(data.uuid(), data);
         final Config file = getFile(data.uuid());
         data.saveToConfig(file);
     }
